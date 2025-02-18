19cc7fbf5f1231479ba162ffd3faf95abcf58dae
David Tad <tadevdavid@gmail.com>
2025-02-18 17:55:07 +0100

    Stilistische verbesserungen

--- Aufgabe 1.R
+++ Aufgabe 1.R
@@ -1,5 +1,5 @@
 #Titanic Datensatz laden
-# sep= ";" wird verwendet bei mir, da die SPalten in der Ursprünglichen 
+# sep= ";" wird verwendet bei mir, da die Spalten in der Ursprünglichen 
 # Datei nicht erkannt werden
 titanic <- 
   read.csv("C:/Users/Miche/Desktop/Wissenschaftliches_Arbeiten/titanic.csv", 
@@ -10,12 +10,14 @@ titanic <-
 # Extrahiert aus dem Namen eine Variable mit der Anrede der Person
 
   
-  # erfasst alles zwischen Komma und Punkt in der Spalte "Name" und packt es in die Spalte "Title",
+  # erfasst alles zwischen Komma und Punkt in der Spalte "Name" und packt es in 
+  # die Spalte "Title",
   # also seperiert den Titel aus der Spalte "Name".
   titanic$Title <- sub(".*,\\s*(.*?)\\..*", "\\1", titanic$Name) 
   
   
-  # Anreden standardisieren, so dass nurnoch Mr, Mrs, Miss und Master verwendet werden
+  # Anreden standardisieren, so dass nurnoch Mr, Mrs, Miss und Master
+  # verwendet werden
   Standart <- c(
     "Ms" = "Mrs",
     "Mlle" = "Mrs",
@@ -36,7 +38,8 @@ titanic <-
     "Major"="Mr"
     )
   
-  #Ändert die Einträge in der Spalte Titel nach dem Muster von der Variabel "Standart"
+  # Aendert die Einträge in der Spalte Titel nach dem Muster von der
+  # Variabel "Standart"
   titanic$Title <- ifelse(titanic$Title %in% names(Standart), 
                           Standart[titanic$Title], 
                           titanic$Title) 
