ab4220353eaf0dde4b473a8083cbf37bda67cdee
David Tad <tadevdavid@gmail.com>
2025-02-18 21:04:35 +0100

    Teilaufgabe 4 in bearbeitung

--- Aufgabe 1.R
+++ Aufgabe 1.R
@@ -9,45 +9,44 @@ titanic <-
 # 1. Teilaufgabe 
 # Extrahiert aus dem Namen eine Variable mit der Anrede der Person
 
-  
-  # erfasst alles zwischen Komma und Punkt in der Spalte "Name" und packt es in 
-  # die Spalte "Title",
-  # also seperiert den Titel aus der Spalte "Name".
-  titanic$Title <- sub(".*,\\s*(.*?)\\..*", "\\1", titanic$Name) 
-  
-  
-  # Anreden standardisieren, so dass nurnoch Mr, Mrs, Miss und Master
-  # verwendet werden
-  Standart <- c(
-    "Ms" = "Mrs",
-    "Mlle" = "Mrs",
-    "Mme" = "Mrs",
-    "Lady" = "Mrs",
-    "Master"="Master",
-    "Mrs"="Mrs",
-    "Mr"="Mr",
-    "Miss"="Miss",
-    "Dr"="Mr",
-    "the Countess"="Mrs",
-    "Rev"="Mr",
-    "Sir"="Mr",
-    "Capt"="Mr",
-    "Col"="Mr",
-    "Don"="Mr",
-    "Jonkheer"="Mr",
-    "Major"="Mr"
-    )
-  
-  # Aendert die Einträge in der Spalte Titel nach dem Muster von der
-  # Variabel "Standart"
-  titanic$Title <- ifelse(titanic$Title %in% names(Standart), 
-                          Standart[titanic$Title], 
-                          titanic$Title) 
-  
+
+# erfasst alles zwischen Komma und Punkt in der Spalte "Name" und packt es in 
+# die Spalte "Title",
+# also seperiert den Titel aus der Spalte "Name".
+titanic$Title <- sub(".*,\\s*(.*?)\\..*", "\\1", titanic$Name) 
+
+
+# Anreden standardisieren, so dass nurnoch Mr, Mrs, Miss und Master
+# verwendet werden
+Standart <- c(
+  "Ms" = "Mrs",
+  "Mlle" = "Mrs",
+  "Mme" = "Mrs",
+  "Lady" = "Mrs",
+  "Master"="Master",
+  "Mrs"="Mrs",
+  "Mr"="Mr",
+  "Miss"="Miss",
+  "Dr"="Mr",
+  "the Countess"="Mrs",
+  "Rev"="Mr",
+  "Sir"="Mr",
+  "Capt"="Mr",
+  "Col"="Mr",
+  "Don"="Mr",
+  "Jonkheer"="Mr",
+  "Major"="Mr"
+)
+
+# Aendert die Einträge in der Spalte Titel nach dem Muster von der
+# Variabel "Standart"
+titanic$Title <- ifelse(titanic$Title %in% names(Standart), 
+                        Standart[titanic$Title], 
+                        titanic$Title) 
+
 # 2. Teilaufgabe (David)
 # Codiert die Variablen „Survived“, „Sex“, „Embarked“ als factor um.
-  
-  titanic$Survived <- as.factor(titanic$Survived)
-  titanic$Sex <- as.factor(titanic$Sex)
-  titanic$Embarked <- as.factor(titanic$Embarked)
-  
+
+titanic$Survived <- as.factor(titanic$Survived)
+titanic$Sex <- as.factor(titanic$Sex)
+titanic$Embarked <- as.factor(titanic$Embarked)
