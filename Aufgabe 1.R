74e347dce0f1d5e52f4998afd1cb3065d889d1c8
Michel <86248758+Michel-IMP@users.noreply.github.com>
2025-02-18 18:47:02 +0100

    Development (#16)
    * Dev. michel (#12)
    
    * 1.Teilaufgabe der ersten Aufgabe angefangen
    
    * 1. Teilaufgabe wurde beendet und mit dem Befehl titanic$Title getestet. Der Code scheint zu funktionieren
    
    * Dev. david (#15)
    
    * Init commit
    
    * Fertige Teilaufgabe 2
    
    * Stilistische verbesserungen
    
    * 3. Teilaufgabe beendet
    
    * Nichts geaendert
    
    ---------
    
    Co-authored-by: David Tad <147756771+dafit77@users.noreply.github.com>

--- Aufgabe 1.R
+++ Aufgabe 1.R
@@ -1,4 +1,25 @@
 #Titanic Datensatz laden
+ Development
+# sep= ";" wird verwendet bei mir, da die Spalten in der Ursprünglichen 
+# Datei nicht erkannt werden
+titanic <- 
+  read.csv("C:/Users/Miche/Desktop/Wissenschaftliches_Arbeiten/titanic.csv", 
+           sep=";")
+
+
+# 1. Teilaufgabe (Michel)
+# Extrahiert aus dem Namen eine Variable mit der Anrede der Person
+
+  
+  # erfasst alles zwischen Komma und Punkt in der Spalte "Name" und packt es in 
+  # die Spalte "Title",
+  # also seperiert den Titel aus der Spalte "Name".
+  titanic$Title <- sub(".*,\\s*(.*?)\\..*", "\\1", titanic$Name) 
+  
+  
+  # Anreden standardisieren, so dass nurnoch Mr, Mrs, Miss und Master
+  # verwendet werden
+
 #sep= ";" wird verwendet bei mir, da die SPalten in der Ursprünglichen Datei nicht erkannt werden
 titanic <- read.csv("C:/Users/Miche/Desktop/Wissenschaftliches_Arbeiten/titanic.csv", sep=";")
 
@@ -13,6 +34,7 @@ titanic <- read.csv("C:/Users/Miche/Desktop/Wissenschaftliches_Arbeiten/titanic.
   
   
   # Anreden standardisieren, so dass nurnoch Mr, Mrs, Miss und Master verwendet werden
+Dev.-Michel
   Standart <- c(
     "Ms" = "Mrs",
     "Mlle" = "Mrs",
@@ -33,11 +55,29 @@ titanic <- read.csv("C:/Users/Miche/Desktop/Wissenschaftliches_Arbeiten/titanic.
     "Major"="Mr"
     )
   
+Development
+  # Aendert die Einträge in der Spalte Titel nach dem Muster von der
+  # Variabel "Standart"
+
   #Ändert die Einträge in der Spalte Titel nach dem Muster von der Variabel "Standart"
+Dev.-Michel
   titanic$Title <- ifelse(titanic$Title %in% names(Standart), 
                           Standart[titanic$Title], 
                           titanic$Title) 
   
+ Development
+# 2. Teilaufgabe (David)
+# Codiert die Variablen „Survived“, „Sex“, „Embarked“ als factor um.
+  
+  titanic$Survived <- as.factor(titanic$Survived)
+  titanic$Sex <- as.factor(titanic$Sex)
+  titanic$Embarked <- as.factor(titanic$Embarked)
+
+# 3. Teilaufgabe (Michel)
+#Überführt die Variable „Pclass“ in einen ordered-factor.
+  titanic$Pclass<-factor(titanic$Pclass, levels = c(3, 2, 1), ordered = TRUE)
+
     #überprüfen ob alle Titel geändert wurden
     titanic$Title
     
+ Dev.-Michel
