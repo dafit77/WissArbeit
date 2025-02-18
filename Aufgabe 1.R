df5f128bd4deb0a4532739a3d71cc474ac5360cb
Michel <86248758+Michel-IMP@users.noreply.github.com>
2025-02-18 21:54:07 +0100

    Development (#20)
    * Dev. michel (#17)
    
    * Development (#16)
    
    * Dev. michel (#12)
    
    * 1.Teilaufgabe der ersten Aufgabe angefangen
    
    * 1. Teilaufgabe wurde beendet und mit dem Befehl titanic$Title getestet. Der Code scheint zu funktionieren
    
    * Dev. david (#15)
    
    * Init commit
    
    * Fertige Teilaufgabe 2
    
    * Stilistische verbesserungen
    
    * 3. Teilaufgabe beendet
    
    ---------
    
    Co-authored-by: David Tad <147756771+dafit77@users.noreply.github.com>
    
    * Kommentare verbessert
    
    * Dev. david (#19)
    
    * Init commit
    
    * Fertige Teilaufgabe 2
    
    * Stilistische verbesserungen
    
    * Teilaufgabe 4 in bearbeitung
    
    * (nochmal) Berichtigter commit mit ein paar stilistischen Aenderungen
    
    * Fertiger Code für Teilaufgabe 4, anpassung read.csv für MacOS
    
    * Berichtigung TA 4 und verbesserungen (fin)
    
    * 1 leere Zeile entfernt
    
    ---------
    
    Co-authored-by: David Tad <147756771+dafit77@users.noreply.github.com>

--- Aufgabe 1.R
+++ Aufgabe 1.R
@@ -1,58 +1,102 @@
 #Titanic Datensatz laden
 # sep= ";" wird verwendet bei mir, da die Spalten in der Ursprünglichen 
 # Datei nicht erkannt werden
+
 titanic <- 
   read.csv("C:/Users/Miche/Desktop/Wissenschaftliches_Arbeiten/titanic.csv", 
            sep=";")
 
+# titanic <- read.csv("~/Documents/R/titanic.csv")
+# Read Datei für mein Mac (David und Colin)
 
 # 1. Teilaufgabe (Michel)
 # Extrahiert aus dem Namen eine Variable mit der Anrede der Person
-  
-  # erfasst alles zwischen Komma und Punkt in der Spalte "Name" und packt es in 
-  # die Spalte "Title",
-  # also seperiert den Titel aus der Spalte "Name".
-  titanic$Title <- sub(".*,\\s*(.*?)\\..*", "\\1", titanic$Name) 
-  
-  
-  # Anreden standardisieren, so dass nurnoch Mr, Mrs, Miss und Master
-  # verwendet werden so dass nurnoch Mr, Mrs, Miss und Master verwendet werden
 
-  Standart <- c(
-    "Ms" = "Mrs",
-    "Mlle" = "Mrs",
-    "Mme" = "Mrs",
-    "Lady" = "Mrs",
-    "Master"="Master",
-    "Mrs"="Mrs",
-    "Mr"="Mr",
-    "Miss"="Miss",
-    "Dr"="Mr",
-    "the Countess"="Mrs",
-    "Rev"="Mr",
-    "Sir"="Mr",
-    "Capt"="Mr",
-    "Col"="Mr",
-    "Don"="Mr",
-    "Jonkheer"="Mr",
-    "Major"="Mr"
-    )
-  
-  # Aendert die Einträge in der Spalte Titel nach dem Muster von der
-  # Variabel "Standart"
-  titanic$Title <- ifelse(titanic$Title %in% names(Standart), 
-                          Standart[titanic$Title], 
-                          titanic$Title) 
 
-  #überprüfen ob alle Titel geändert wurden
-  titanic$Title  
+# erfasst alles zwischen Komma und Punkt in der Spalte "Name" und packt es in 
+# die Spalte "Title",
+# also seperiert den Titel aus der Spalte "Name".
+
+titanic$Title <- sub(".*,\\s*(.*?)\\..*", "\\1", titanic$Name) 
+# table(titanic$Title) # Anzeigen, welche Titel existieren 
+                       # und welche ersetzt werden
+
+# Anreden standardisieren, so dass nurnoch Mr, Mrs, Miss und Master
+# verwendet werden
+
+Standart <- c(
+  "Ms" = "Mrs",
+  "Mlle" = "Mrs",
+  "Mme" = "Mrs",
+  "Lady" = "Mrs",
+  "Master"="Master",
+  "Mrs"="Mrs",
+  "Mr"="Mr",
+  "Miss"="Miss",
+  "Dr"="Mr",
+  "the Countess"="Mrs",
+  "Rev"="Mr",
+  "Sir"="Mr",
+  "Capt"="Mr",
+  "Col"="Mr",
+  "Don"="Mr",
+  "Jonkheer"="Mr",
+  "Major"="Mr"
+)
+
+# Aendert die Einträge in der Spalte Titel nach dem Muster von der
+# Variabel "Standart"
+
+titanic$Title <- ifelse(titanic$Title %in% names(Standart), 
+                        Standart[titanic$Title], 
+                        titanic$Title) 
+
+#überprüfen ob alle Titel geändert wurden
+
+titanic$Title  
 
 # 2. Teilaufgabe (David)
 # Codiert die Variablen „Survived“, „Sex“, „Embarked“ als factor um.
-  titanic$Survived <- as.factor(titanic$Survived)
-  titanic$Sex <- as.factor(titanic$Sex)
-  titanic$Embarked <- as.factor(titanic$Embarked)
+
+titanic$Survived <- as.factor(titanic$Survived)
+titanic$Sex <- as.factor(titanic$Sex)
+titanic$Embarked <- as.factor(titanic$Embarked)
 
 # 3. Teilaufgabe (Michel)
-#Überführt die Variable „Pclass“ in einen ordered-factor.
-  titanic$Pclass<-factor(titanic$Pclass, levels = c(3, 2, 1), ordered = TRUE)
+# Überführt die Variable „Pclass“ in einen ordered-factor.
+
+titanic$Pclass<-factor(titanic$Pclass, levels = c(3, 2, 1), ordered = TRUE)
+
+# 4. Teilaufgabe (David)
+# Imputiert fehlende Werte in der Variable „Age“ mithilfe der erzeugten
+# Variable „Anrede“ über ein Imputationsverfahren eurer Wahl (z.B.
+# arithmetisches Mittel, Median, usw.)
+
+med.Mr <- median(titanic$Age[titanic$Title == "Mr"], na.rm = TRUE)
+med.Mrs <- median(titanic$Age[titanic$Title == "Mrs"], na.rm = TRUE)
+med.Master <- median(titanic$Age[titanic$Title == "Master"], na.rm = TRUE)
+med.Miss <- median(titanic$Age[titanic$Title == "Miss"], na.rm = TRUE)
+# na.rm funkrioniert vlcht bei Michel nicht, nachher bitte einmal testen
+
+allMr <- titanic$Age[titanic$Title == "Mr"]
+FalseVek1 <- is.na(titanic$Age[titanic$Title == "Mr"])
+allMr[FalseVek1] <- med.Mr
+titanic$Age[titanic$Title == "Mr"] <- allMr
+
+allMrs <- titanic$Age[titanic$Title == "Mrs"]
+FalseVek2 <- is.na(titanic$Age[titanic$Title == "Mrs"])
+allMrs[FalseVek2] <- med.Mrs
+titanic$Age[titanic$Title == "Mrs"] <- allMrs
+
+allMaster <- titanic$Age[titanic$Title == "Master"]
+FalseVek3 <- is.na(titanic$Age[titanic$Title == "Master"])
+allMaster[FalseVek3] <- med.Master
+titanic$Age[titanic$Title == "Master"] <- allMaster
+
+allMiss <- titanic$Age[titanic$Title == "Miss"]
+FalseVek3 <- is.na(titanic$Age[titanic$Title == "Miss"])
+allMiss[FalseVek3] <- med.Miss
+titanic$Age[titanic$Title == "Miss"] <- allMiss 
+
+  
+ 
