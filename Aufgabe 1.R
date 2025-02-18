850d7bfbe4757e7ddda3440c1f9eb3e52f7cc214
Michel <86248758+Michel-IMP@users.noreply.github.com>
2025-02-18 15:05:28 +0100

    Dev. michel (#12)
    * 1.Teilaufgabe der ersten Aufgabe angefangen
    
    * 1. Teilaufgabe wurde beendet und mit dem Befehl titanic$Title getestet. Der Code scheint zu funktionieren

---
+++ Aufgabe 1.R
@@ -0,0 +1,39 @@
+#Titanic Datensatz laden
+#sep= ";" wird verwendet bei mir, da die SPalten in der Ursprünglichen Datei nicht erkannt werden
+titanic <- read.csv("C:/Users/Miche/Desktop/Wissenschaftliches_Arbeiten/titanic.csv", sep=";")
+
+
+#1. Teilaufgabe 
+#Extrahiert aus dem Namen eine Variable mit der Anrede der Person
+
+  
+  #erfasst alles zwischen Komma und Punkt in der Spalte "Name" und packt es in die Spalte "Title",
+  #also seperiert den Titel aus der Spalte "Name".
+  titanic$Title <- sub(".*,\\s*(.*?)\\..*", "\\1", titanic$Name) 
+  
+  
+  # Anreden standardisieren, so dass nurnoch Mr, Mrs, Miss und Master verwendet werden
+  Standart <- c(
+    "Ms" = "Mrs",
+    "Mlle" = "Mrs",
+    "Mme" = "Mrs",
+    "Lady" = "Mrs",
+    "Master"="Master",
+    "Mrs"="Mrs",
+    "Mr"="Mr",
+    "Miss"="Miss",
+    "Dr"="Mr",
+    "the Countess"="Mrs",
+    "Rev"="Mr",
+    "Sir"="Mr",
+    "Capt"="Mr",
+    "Col"="Mr",
+    "Don"="Mr",
+    "Jonkheer"="Mr",
+    "Major"="Mr"
+    )
+  
+  #Ändert die Einträge in der Spalte Titel nach dem Muster von der Variabel "Standart"
+  titanic$Title <- ifelse(titanic$Title %in% names(Standart), 
+                          Standart[titanic$Title], 
+                          titanic$Title) 
