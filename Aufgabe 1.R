677c91d07b59a7a1fd980963424952a75ee237a8
David Tad <tadevdavid@gmail.com>
2025-02-19 10:39:27 +0100

    Aufgabe 1 vollständig bearbeitet + Einige stilistische Verbesserungen

--- Aufgabe 1.R
+++ Aufgabe 1.R
@@ -1,28 +1,27 @@
 #Titanic Datensatz laden
-# sep= ";" wird verwendet bei mir, da die Spalten in der Ursprünglichen 
+# sep= ";" wird verwendet bei mir, da die Spalten in der Urspruenglichen 
 # Datei nicht erkannt werden
 
-titanic <- 
-  read.csv("C:/Users/Miche/Desktop/Wissenschaftliches_Arbeiten/titanic.csv", 
+titanic <-
+  read.csv("C:/Users/Miche/Desktop/Wissenschaftliches_Arbeiten/titanic.csv",
            sep=";")
 
 # titanic <- read.csv("~/Documents/R/titanic.csv")
-# Read Datei für mein Mac (David und Colin)
+# Read Datei fuer mein Mac (David und Colin)
 
 # 1. Teilaufgabe (Michel)
 # Extrahiert aus dem Namen eine Variable mit der Anrede der Person
 
+
 # erfasst alles zwischen Komma und Punkt in der Spalte "Name" und packt es in 
 # die Spalte "Title",
 # also seperiert den Titel aus der Spalte "Name".
-
 titanic$Title <- sub(".*,\\s*(.*?)\\..*", "\\1", titanic$Name) 
 # table(titanic$Title) # Anzeigen, welche Titel existieren 
-                       # und welche ersetzt werden
+# und welche ersetzt werden
 
 # Anreden standardisieren, so dass nurnoch Mr, Mrs, Miss und Master
 # verwendet werden
-
 Standart <- c(
   "Ms" = "Mrs",
   "Mlle" = "Mrs",
@@ -43,16 +42,16 @@ Standart <- c(
   "Major"="Mr"
 )
 
-# Aendert die Einträge in der Spalte Titel nach dem Muster von der
+# Aendert die Eintraege in der Spalte Titel nach dem Muster von der
 # Variabel "Standart"
 
 titanic$Title <- ifelse(titanic$Title %in% names(Standart), 
                         Standart[titanic$Title], 
                         titanic$Title) 
 
-#überprüfen ob alle Titel geändert wurden
+# ueberpruefen ob alle Titel geaendert wurden
+# table(titanic$Title)  
 
-titanic$Title  
 
 # 2. Teilaufgabe (David)
 # Codiert die Variablen „Survived“, „Sex“, „Embarked“ als factor um.
@@ -62,13 +61,13 @@ titanic$Sex <- as.factor(titanic$Sex)
 titanic$Embarked <- as.factor(titanic$Embarked)
 
 # 3. Teilaufgabe (Michel)
-# Überführt die Variable „Pclass“ in einen ordered-factor.
+# Ueberfuehrt die Variable „Pclass“ in einen ordered-factor.
 
 titanic$Pclass<-factor(titanic$Pclass, levels = c(3, 2, 1), ordered = TRUE)
 
 # 4. Teilaufgabe (David)
 # Imputiert fehlende Werte in der Variable „Age“ mithilfe der erzeugten
-# Variable „Anrede“ über ein Imputationsverfahren eurer Wahl (z.B.
+# Variable „Anrede“ ueber ein Imputationsverfahren eurer Wahl (z.B.
 # arithmetisches Mittel, Median, usw.)
 
 med.Mr <- median(titanic$Age[titanic$Title == "Mr"], na.rm = TRUE)
@@ -76,6 +75,7 @@ med.Mrs <- median(titanic$Age[titanic$Title == "Mrs"], na.rm = TRUE)
 med.Master <- median(titanic$Age[titanic$Title == "Master"], na.rm = TRUE)
 med.Miss <- median(titanic$Age[titanic$Title == "Miss"], na.rm = TRUE)
 # na.rm funkrioniert vlcht bei Michel nicht, nachher bitte einmal testen
+# funktioniert (Michel)
 
 allMr <- titanic$Age[titanic$Title == "Mr"]
 FalseVek1 <- is.na(titanic$Age[titanic$Title == "Mr"])
@@ -96,3 +96,51 @@ allMiss <- titanic$Age[titanic$Title == "Miss"]
 FalseVek3 <- is.na(titanic$Age[titanic$Title == "Miss"])
 allMiss[FalseVek3] <- med.Miss
 titanic$Age[titanic$Title == "Miss"] <- allMiss 
+
+
+# 5. Teilaufgabe
+# Extrahiert aus der Variable „Cabin“ die folgenden Informationen und erzeugt
+# neue Variablen hierfür:
+
+# 5.3
+# Eintraege mit unbekannter Kabinennummer, d.h. „“ setzt ihr auf NA.
+titanic$Cabin[titanic$Cabin == ""] <- NA # 5.3 zuerst, damit Komplikationen
+                                         # im Code bei 5.1 und 5.2 vermieden
+                                         # werden
+
+# 5.1
+# Backbord oder Steuerbord? Tipp: Kabinen mit einer ungeraden Nummer liegen
+# auf Steuerbord, die anderen auf Backbord.
+titanic$Side <-  ifelse(
+  is.na(titanic$Cabin), NA, ifelse(as.numeric(gsub("[^0-9]", "",
+                                                  titanic$Cabin)) %% 2 == 1, 
+                                 # Ueberprueft ob Kabinennummer modolu 2 gleich
+                                 # 1 ist, also ob Kabinennummer ungerade ist
+                                                  "Steuerbord",
+                                 # Wenn Ungerade wird Wert in Spalte Side zu
+                                 # Steuerbord gesetzt
+                                                  "Backbord"
+                                 # Ansonsten auf Backbord gesetzt
+                                  )
+)
+
+# 5.2
+# Deck: Vorangehender Buchstabe der Kabinennummer
+titanic$Deck<-ifelse(
+  is.na(titanic$Cabin), NA,      # Wenn Kabinennummer unbekannt ist so
+                                 # ist auch Decknummer unbekannt
+  substr(titanic$Cabin, 1, 1)    # Ansonsten nimmt es den ersten Wert 
+                                 # der Kabinennummer
+)
+
+# 6
+# Entfernt am Ende die Variablen „PassengerID“, „Name“,
+# „Ticket“ und „Cabin“ aus dem Datensatz
+
+titanic$PassengerId <- NULL
+titanic$Name <- NULL
+titanic$Ticket <- NULL
+titanic$Cabin <- NULL
+
+# Abspeichern
+write.csv(titanic, file = "titanic_Berichtigt.csv", row.names = FALSE)
