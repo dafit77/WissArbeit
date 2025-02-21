f77ee43dbb5b9c8c1a764bfcf6435a0d23107d17
ColinM2000 <colin.morgenstern@tu-dortmund.de>
2025-02-21 14:49:08 +0100

    Update Skript1 Aufgabe 2a iii, iv.R

--- Skript1 Aufgabe 2a iii, iv.R
+++ Skript1 Aufgabe 2a iii, iv.R
@@ -43,42 +43,33 @@ bivariate_stats <- function(data, var1, var2) {
 bivariate_stats(titanic_Berichtigt,"Survived", "Sex")
 
 
-
-##
-bivariate_stats <- function(data, var1, var2) {
+bivariate_statsfkt <- function(data, dichotom_var, metric_var) {
   library(dplyr)
-  library(gmodels)  # Für CrossTable
-  library(DescTools) # Für den Phi-Koeffizienten und Punktbiseriale Korrelation
+  library(psych)  # Für Punktbiseriale Korrelation
   
-  # Prüfen, ob var1 dichotom und var2 metrisch ist
-  if (length(unique(data[[var1]])) == 2 && is.numeric(data[[var2]])) {
-    cat("\nPunktbiseriale Korrelation:\n")
-    point_biserial <- cor(data[[var2]], as.numeric(as.factor(data[[var1]])), method = "pearson")
-    print(point_biserial)
+  
+  # Prüfen, ob die Variablen korrekt sind
+  if (!(dichotom_var %in% names(data)) || !(metric_var %in% names(data))) {
+    stop("Eine oder beide Variablen existieren nicht im Datensatz.")
   }
   
-  # Kontingenztabelle
-  cat("\nKontingenztabelle:\n")
-  tbl <- table(data[[var1]], data[[var2]])
-  print(tbl)
-  
-  # Relative Häufigkeiten
-  cat("\nRelative Häufigkeiten (gesamt):\n")
-  print(prop.table(tbl))
-  
-  cat("\nRelative Häufigkeiten (zeilenweise):\n")
-  print(prop.table(tbl, margin = 1))
-  
-  cat("\nRelative Häufigkeiten (spaltenweise):\n")
-  print(prop.table(tbl, margin = 2))
-  
-  # Phi-Koeffizient (nur für dichotome Variablen)
-  if (length(unique(data[[var1]])) == 2 && length(unique(data[[var2]])) == 2) {
-    cat("\nPhi-Koeffizient:\n")
-    phi_value <- Phi(tbl)
-    print(phi_value)
-  } else {
-    cat("\nPhi-Koeffizient nicht berechnet: Mindestens eine Variable hat mehr als zwei Kategorien.\n")
+  if (length(unique(data[[dichotom_var]])) != 2) {
+    stop("Die erste Variable muss dichotom sein (nur zwei eindeutige Werte).")
   }
-}
+  
+  if (!is.numeric(data[[metric_var]])) {
+    stop("Die zweite Variable muss metrisch sein.")
+  }
+  
+  # Punktbiseriale Korrelation berechnen
+  cat("\nPunktbiseriale Korrelation:\n")
+  point_biserial <- biserial(data[[metric_var]], as.numeric(as.factor(data[[dichotom_var]])))
+  print(point_biserial)
+} 
+ 
+
+# Beispielaufruf mit Titanic-Daten:
+# bivariate_stats(titanic_data, "Survived", "Age")
+
+bivariate_statsfkt(titanic_Berichtigt,"Survived", "Age")
 
