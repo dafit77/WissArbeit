992b9e0a96c7fde07899de318c65c8a0524b72a3
maxi2048 <maximilian.petzel@icloud.com>
2025-02-22 18:19:35 +0100

    Source Error

---
+++ Aufgabe4.R
@@ -0,0 +1,14 @@
+# Aufgabenstellung von Aufgabe 4
+# Diejenigen Gruppenmitglieder, die nicht an (1.) gearbeitet haben,
+# sollen mit Hilfe der Funktionen den aufgeräumten Datensatz aus (1.) analysieren
+# (Deskription und Visualisierung). 
+
+# Funktionen aus den Teilskripten laden
+
+source("Aufgabe_2_Skript_1.R")
+source("R_Skript1.R")
+source("Skript1_Aufgabe_2a_iii_iv.R")
+# Sourcing schlägt fehl
+file.exists("Skript1_Aufgabe_2a_iii_iv.R")
+source(list.files(pattern = "Skript1_Aufgabe_2a_iii_iv.R$", full.names = TRUE))
+source("Skript1_Aufgabe_2a_iii_iv.R", encoding = "UTF-8")
