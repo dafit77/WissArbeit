055810e1b40c104103f524de08ae0cfd97a19f2a
maxi2048 <maximilian.petzel@icloud.com>
2025-02-23 03:05:11 +0100

    Late Night

--- Aufgabe4.R
+++ Aufgabe4.R
@@ -5,14 +5,13 @@
 
 # Funktionen aus den Teilskripten laden
 
-source("Aufgabe_2_Skript_1.R")
-source("R_Skript1.R")
-source("Skript1_Aufgabe_2a_iii_iv.R")
-# Sourcing schlägt fehl
-file.exists("Skript1_Aufgabe_2a_iii_iv.R")
-source(list.files(pattern = "Skript1_Aufgabe_2a_iii_iv.R$", full.names = TRUE))
-source("Skript1_Aufgabe_2a_iii_iv.R", encoding = "UTF-8")
+# renv für saubere Projekt Library
+renv::activate()
+renv::restore(prompt = FALSE) 
 
+source("Funktionen_Aufgabe2/Aufgabe_2_Skript_1.R")
+source("Funktionen_Aufgabe2/R_Skript1.R")
+source("Funktionen_Aufgabe2/Aufgabe2_3_Skript1.R")
 # Analyse des titanic_Bereinigt.csv
 
 df <- read.csv("titanic.csv", header = TRUE, stringsAsFactors = FALSE)
@@ -22,16 +21,30 @@ df
 # Wie ist das Alter der Gäste auf der Titanic verteilt gewesen ? 
 func_spread_summary(df$Age)
 # Unterscheidet sich das Alter von Überlebenden und Verstorbenen ? 
-# These: Alte Menschen und Kinder 
+# These: Alte Menschen und Kinder können nicht so gut schwimmen/ erfrieren schnell
+# Im Eiswasser, auch wenn auf Ältere und Kinder bei der Rettung besondere Rücksicht 
+# genommen wird sterben 
 
 func_spread_summary(df[df$Survived == 0,]$Age)
 func_spread_summary(df[df$Survived == 1,]$Age)
-# Wie ist sind mehr Frauen oder mehr Männer auf dem Schiff gewesen ? 
+# sind mehr Frauen oder mehr Männer auf dem Schiff gewesen ? 
 func_categorial_summary(df$Sex)
 # Mehr Männer 
 # Wie sieht die Aufteilung bei den Überlebenden aus ? 
 func_categorial_summary(df[df$Survived == 1,]$Sex)
+# Mehr Frauen
 
 
 
+# Wieviele Tickets jeder Klasse wurden gekauft ? 
+# Zu Geschlecht und Survived wurden ja schon deskriptive Statistiken berechnet
+Visualisierung(df, "Sex", "Survived", "Pclass")
 
+
+bivariate_stats(df, "Survived", "Sex")
+# Hypothese: Vor allem die Gäste aus den günstigeren Klassen sind verstorben
+bivariate_statsfkt(df, "Survived", "Pclass")
+# Indiz für ja, da negative Korrelation zwischen Survived und Pclass
+# Frage: Wer hatte eine bessere Überlebenschance Junge oder Alte Menschen ? 
+bivariate_statsfkt(df, "Survived", "Age")
+# Junge Menschen haben eine bessere Chance gehabt 
